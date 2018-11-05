e187c90d6bfb6e6ab903367808efdce92e046714
tamuramasatoshi <rx88sp@gmail.com>
2018-11-05 18:33:37 +0900

    11月5日　未完にて

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,17 +9,113 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var nextbu: UIButton!
+    @IBOutlet weak var back: UIButton!
+    
+       
+    var timer : Timer!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageview.image = UIImage(named:"IMG_1165.JPG")
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //画像をタップした時に画面遷移
+    @IBAction func ontapimage(_ sender: Any) {
+    
+        performSegue(withIdentifier: "result", sender: nil)
+        
+        
+       // let zoomimage.image = imageview.image
+        
+    }
+   
+    var  imageNumber = 0
+    //進むのボタンを押した時
+    @IBAction func nextbu(_ sender: Any) {
+        
+        if imageNumber == 0 {
+            imageview.image = UIImage(named:"IMG_1168.JPG")
+            imageNumber = imageNumber + 1
+            
+        } else if imageNumber == 1 {
+            imageview.image = UIImage(named:"IMG_1172.JPG")
+            imageNumber = imageNumber + 1
+            
+        } else if imageNumber == 2 {
+            imageview.image = UIImage(named:"IMG_1165.JPG")
+            imageNumber = 0
+        }
+        
+    }
+    
+    //戻るのボタンを押した時
+    @IBAction func back(_ sender: Any) {
+        
+        
+        if imageNumber == 0 {
+            imageview.image = UIImage(named:"IMG_1168.JPG")
+            imageNumber = 2
+            
+        } else if imageNumber == 1 {
+            imageview.image = UIImage(named:"IMG_1172.JPG")
+            imageNumber = 0
+            
+        } else if imageNumber == 2 {
+            imageview.image = UIImage(named:"IMG_1165.JPG")
+            imageNumber = 1
+        }
+        
+    }
+    //スライドショーで行う内容
+    @objc func updateTimer(_ timer: Timer) {
+        
+        if imageNumber == 0 {
+            imageview.image = UIImage(named:"IMG_1168.JPG")
+            imageNumber = imageNumber + 1
+            
+        } else if imageNumber == 1 {
+            imageview.image = UIImage(named:"IMG_1172.JPG")
+            imageNumber = imageNumber + 1
+            
+        } else if imageNumber == 2 {
+            imageview.image = UIImage(named:"IMG_1165.JPG")
+            imageNumber = 0
+        }
+    }
+    //再生・一時停止のボタンを押した時
+    @IBAction func timerstart(_ sender: Any) {
+        
+        if self.timer == nil {
+            
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0 , target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            
+            nextbu.isEnabled = false
+            back.isEnabled = false
+            
+        }
+        else if self.timer !== nil {
+            
+            self.timer.invalidate()
+            self.timer = nil
+            nextbu.isEnabled = true
+            back.isEnabled = true
+            
+        }
+        
+    }
 }
 
---
+++ SlideshowApp/ZoomViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ZoomViewController.swift
+//  SlideshowApp
+//
+//  Created by 田村尚利 on 2018/11/03.
+//  Copyright © 2018 masatoshi.tamura. All rights reserved.
+//
+
+import UIKit
+
+class ZoomViewController: UIViewController {
+
+    
+ @IBOutlet weak var zoomimage: UIImageView!
+    
+   override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
